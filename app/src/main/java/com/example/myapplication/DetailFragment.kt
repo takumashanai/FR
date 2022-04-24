32afed0f6722bd6ef43527213d6aa623ad23b3c1
adeki <adekiuko@gmail.com>
2022-04-25 01:42:54 +0900

    日曜進捗 最終

--- app/src/main/java/com/example/myapplication/DetailFragment.kt
+++ app/src/main/java/com/example/myapplication/DetailFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.example.myapplication.databinding.FragmentDetailBinding
 
 
@@ -13,6 +14,7 @@ class DetailFragment(
 ): Fragment() {
     private var _binding: FragmentDetailBinding? = null
     private val binding get() = _binding!!
+    private val sharedViewModel: GitHubUserViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
